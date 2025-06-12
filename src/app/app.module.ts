ca76764dd41672a3d79d23d25ed6d90b0f91095e
minorusal <minorusal@hotmail.com>
2025-06-12 10:20:57 -0600

    Fix: se arregla la version correcta de swagger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+const config: SocketIoConfig = { url: 'ws://localhost:8000', options: {} };
+// const config: SocketIoConfig = { url: 'wss://devsocket.credibusiness.io', options: {} };
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config),
+    HttpClientModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
