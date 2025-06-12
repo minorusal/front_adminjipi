e9f3e7b376bc651959dea3ae8315e01888c48820
Minoru Daniel Salazar Palafox <minorusal@hotmail.com>
2025-06-12 17:03:50 -0600

    Use collapsible list for dashboard menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MatTreeModule } from '@angular/material/tree';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SettingsComponent } from './settings/settings.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, SettingsComponent],
+  declarations: [AppComponent, DashboardComponent, SettingsComponent, HomeComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule,
-    MatTreeModule,
-    MatIconModule,
-    MatButtonModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { NestedTreeControl } from '@angular/cdk/tree';
-import { MatTreeNestedDataSource } from '@angular/material/tree';
 
 export interface MenuNode {
   id: number;
@@ -21,12 +20,10 @@ export class DashboardComponent implements OnInit {
   @Output() logout = new EventEmitter<void>();
   menuOpen = false;
   menuTree: MenuNode[] = [];
-  treeControl = new NestedTreeControl<MenuNode>((node: MenuNode) => node.children);
-  dataSource = new MatTreeNestedDataSource<MenuNode>();
+  expanded: Record<number, boolean> = {};
   private ownerId = 1;
-  selectedView = 'home';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   private getCookie(name: string): string | null {
     const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
@@ -51,18 +48,22 @@ export class DashboardComponent implements OnInit {
         `http://localhost:3000/menus?owner_id=${this.ownerId}`,
         options
       )
-      .subscribe((tree) => {
-        this.menuTree = tree as MenuNode[];
-        this.dataSource.data = this.menuTree;
-      });
+      .subscribe((tree) => (this.menuTree = tree as MenuNode[]));
   }
 
-  hasChild = (_: number, node: MenuNode) =>
-    !!node.children && node.children.length > 0;
+  toggleNode(id: number): void {
+    this.expanded[id] = !this.expanded[id];
+  }
 
-  selectView(view: string): void {
-    this.selectedView = view;
-    this.menuOpen = false;
+  isOpen(id: number): boolean {
+    return !!this.expanded[id];
+  }
+
+  navigateTo(path: string | null | undefined): void {
+    if (path) {
+      this.router.navigate([path]);
+      this.menuOpen = false;
+    }
   }
 
   onLogout(): void {
