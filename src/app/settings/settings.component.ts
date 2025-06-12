2604e82a939bb799aed2f24a8a6881c88b11b502
Minoru Daniel Salazar Palafox <minorusal@hotmail.com>
2025-06-12 16:26:43 -0600

    Simplify menu tree with standard list

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { NestedTreeControl } from '@angular/cdk/tree';
-import { MatTreeNestedDataSource } from '@angular/material/tree';
 
 export interface MenuNode {
   id: number;
@@ -20,8 +18,6 @@ export class SettingsComponent implements OnInit {
   menuForm: FormGroup;
   parentMenus: any[] = [];
   menuTree: MenuNode[] = [];
-  treeControl = new NestedTreeControl<MenuNode>((node: MenuNode) => node.children);
-  dataSource = new MatTreeNestedDataSource<MenuNode>();
   private ownerId = 1;
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
@@ -70,9 +66,6 @@ export class SettingsComponent implements OnInit {
         // return a flat list, so build the hierarchy if "children" are missing.
         const isFlat = tree.length && !tree.some((m) => Array.isArray(m.children));
         this.menuTree = isFlat ? this.buildTree(tree) : (tree as MenuNode[]);
-        this.dataSource.data = this.menuTree;
-        this.treeControl.dataNodes = this.menuTree;
-        this.treeControl.collapseAll();
       });
   }
 
@@ -87,8 +80,6 @@ export class SettingsComponent implements OnInit {
       }));
   }
 
-  hasChild = (_: number, node: MenuNode) =>
-    !!node.children && node.children.length > 0;
 
   onSubmit(): void {
     const { name, url, parent } = this.menuForm.value;
