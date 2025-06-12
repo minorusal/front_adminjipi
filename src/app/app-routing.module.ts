b95c3c8ae4c9f619bca03a4d87a7297c176535e2
Minoru Daniel Salazar Palafox <minorusal@hotmail.com>
2025-06-12 10:29:32 -0600

    feat: show login at root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,12 @@ import { LoginComponent } from './auth/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  // Show the login component when the root path is visited
+  { path: '', component: LoginComponent, pathMatch: 'full' },
+
+  // Alias to access the login using '/login'
   { path: 'login', component: LoginComponent },
+
   { path: 'dashboard', component: DashboardComponent }
 ];
 
