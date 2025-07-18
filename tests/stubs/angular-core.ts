e8389687ace93c47e256685ec0b24614c7ae105f
Minoru Daniel Salazar Palafox <minorusal@hotmail.com>
2025-07-18 11:32:56 -0600

    test: add coverage for ids and notifications

--- tests/stubs/angular-core.ts
+++ tests/stubs/angular-core.ts
@@ -1,3 +1,11 @@
 export function Injectable(_?: any) {
   return function(_: any) {};
 }
+
+export function Component(_?: any) {
+  return function(_: any) {};
+}
+
+export enum ChangeDetectionStrategy {
+  OnPush,
+}
