5ab8e6c69918f5e5d49cc203fc86e4462532f19c
Minoru Daniel Salazar Palafox <minorusal@hotmail.com>
2025-07-18 11:28:44 -0600

    Add notification UI components and service

--- tests/stubs/angular-common-http.ts
+++ tests/stubs/angular-common-http.ts
@@ -6,6 +6,9 @@ export class HttpClient {
   post(_url: string, _body: any, _opts: any) {
     return null as any;
   }
+  get<T>(_url: string, _opts?: any) {
+    return null as any;
+  }
   delete<T>(_url: string, _opts: any) {
     return null as any;
   }
